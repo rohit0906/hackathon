b9ba5a3810ab065de27ca3419d397cd8412d7537
Ratnesh4193 <57043581+Ratnesh4193@users.noreply.github.com>
2020-10-28 23:21:50 +0530

    changed mp3 to wav on line 230

--- script/assistant_gui.py
+++ script/assistant_gui.py
@@ -227,7 +227,7 @@ def respond(voice_data):
             webbrowser.open('www.google.com')
 def engine_speak(audio_string):
     tts=gTTS(text=audio_string,lang='en',slow=False)
-    filename=str(random.randint(1,100110))+"welcome.mp3"
+    filename=str(random.randint(1,100110))+"welcome.wav"
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
