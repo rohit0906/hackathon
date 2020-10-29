5de56889e31c48a89ec28cc9ffafaaf97a8c5e0e
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2020-10-29 15:07:42 +0530

    trying deployment server

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from script import assistant_gui
 
 
@@ -8,16 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def load_home():
     name = "Ratnesh"
-    return render_template('home.html', name= name)
+    return render_template('home.html', name=name)
 
 
-@app.route("/func",methods={'GET','POST'})
+@app.route("/func", methods={'GET', 'POST'})
 def process():
-    name =request.form.get('input')
+    name = request.form.get('input')
     '''name=assistant_gui.record_audio()
     assistant_gui.respond(name)'''
     assistant_gui.respond(name)
     return render_template('home.html', name=name)
 
 
-app.run(debug=True)
+app.run(debug=False)
